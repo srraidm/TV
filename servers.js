454e71351ecc1542e67860b4535844432df794a4
SrRaid <yeraldff2@gmail.com>
2026-02-28 05:16:26 -0300

    Update servers.js

--- servers.js
+++ servers.js
@@ -1,63 +1,60 @@
-/**
- * SERVERS_DB - Base de datos de reproductores 2026
- * Este archivo centraliza todos los enlaces para evitar errores de formato.
- */
-
+/* --- BASE DE DATOS DE SERVIDORES (MIRRORS) --- */
 const SERVERS_DB = {
-    // Generador de enlaces para Anime (utiliza el ID de MyAnimeList)
+    // REPRODUCTORES DE ANIME (Basados en ID de MyAnimeList)
     anime: (id, ep) => ({
-        "Vidsrc.me": `https://vidsrc.me/embed/anime?mal=${id}&ep=${ep}`,
+        "JK-Mirror 1": `https://vidsrc.cc/v2/embed/anime/${id}/${ep}`,
+        "JK-Mirror 2": `https://vidsrc.xyz/embed/anime/${id}?ep=${ep}`,
         "Vidsrc.to": `https://vidsrc.to/embed/anime/${id}/${ep}`,
-        "Vidsrc.xyz": `https://vidsrc.xyz/embed/anime?mal=${id}&ep=${ep}`,
-        "Magi": `https://vidsrc.to/embed/anime/${id}/${ep}`,
-        "SuperEmbed": `https://multiembed.mov/?video_id=${id}&s=${ep}&type=anime`,
+        "Vidsrc.pro": `https://vidsrc.pro/embed/anime/${id}/${ep}`,
         "Embed.su": `https://embed.su/embed/anime/${id}/${ep}`,
-        "Vidhide": `https://vidhide.online/v/${id}`,
-        "VOE.sx": `https://voe.sx/e/${id}`,
-        "Okru": `https://ok.ru/videoembed/${id}`,
-        "Streamwish": `https://awish.pro/e/${id}`,
-        "Filemoon": `https://filemoon.sx/e/${id}`
+        "2Embed": `https://www.2embed.cc/embedanime/${id}/${ep}`,
+        "AnimeFenix": `https://multiembed.mov/?video_id=${id}&s=${ep}&type=anime`,
+        "AutoEmbed": `https://autoembed.to/anime/mal/${id}/${ep}`,
+        "Alpha-Stream": `https://vidsrc.me/embed/anime?mal=${id}&ep=${ep}`
     }),
 
-    // Generador de enlaces para Cine & Series (utiliza el ID de TMDB)
+    // REPRODUCTORES DE CINE Y SERIES (Basados en ID de TMDB)
     cine: (id) => ({
-        "Vidsrc.xyz": `https://vidsrc.xyz/embed/movie?tmdb=${id}`,
-        "Vidsrc.me": `https://vidsrc.me/embed/movie?tmdb=${id}`,
-        "Embed.su": `https://embed.su/embed/movie/${id}`,
-        "Vidhide": `https://vidhide.online/v/${id}`,
-        "Cuevana": `https://api.cuevana.biz/embed/${id}`,
-        "AutoEmbed": `https://autoembed.to/movie/tmdb/${id}`,
-        "Viper": `https://vidsrc.me/embed/movie?tmdb=${id}`,
-        "Uqload": `https://uqload.com/embed/${id}`,
-        "Mixdrop": `https://mixdrop.co/e/${id}`
+        "Latino 1": `https://vidsrc.cc/v2/embed/movie/${id}`,
+        "Latino 2": `https://vidsrc.xyz/embed/movie?tmdb=${id}`,
+        "Castellano": `https://embed.su/embed/movie/${id}`,
+        "Subtitulado": `https://vidsrc.to/embed/movie/${id}`,
+        "Pro-Player": `https://player.autoembed.cc/movie/${id}`,
+        "Nube": `https://nuuub.com/embed/${id}`,
+        "Multi-Mirror": `https://vidsrc.me/embed/movie?tmdb=${id}`
     }),
 
-    // Canales de TV (Enlaces directos HLS)
+    // CANALES DE TV (HLS / M3U8)
     tv: [
         { 
-            n: "Canal 13 Chile", 
+            n: "Canal 13", 
             u: "https://rudo.video/live/c13", 
-            img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Canal_13_Chile_logo.svg/1200px-Canal_13_Chile_logo.svg.png" 
+            img: "https://upload.wikimedia.org/wikipedia/commons/4/40/Canal_13_Chile_logo.svg" 
         },
         { 
-            n: "Mega HD", 
+            n: "Mega", 
             u: "https://rudo.video/live/megatv", 
             img: "https://upload.wikimedia.org/wikipedia/commons/2/22/Mega_logo_2020.png" 
         },
         { 
-            n: "Chilevisión", 
+            n: "CHV", 
             u: "https://rudo.video/live/chv", 
-            img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Chilevisi%C3%B3n_logo_2018.svg/1024px-Chilevisi%C3%B3n_logo_2018.svg.png" 
-        },
-        { 
-            n: "TVN Chile", 
-            u: "https://rudo.video/live/tvn", 
-            img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/TVN_Chile_logo_2020.svg/1200px-TVN_Chile_logo_2020.svg.png" 
+            img: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Chilevisi%C3%B3n_logo_2018.svg" 
         },
         { 
             n: "TNT Sports", 
             u: "https://rudo.video/live/tntsports", 
             img: "https://logodownload.org/wp-content/uploads/2019/11/tnt-sports-logo-0.png" 
+        },
+        { 
+            n: "TVN", 
+            u: "https://rudo.video/live/tvn", 
+            img: "https://upload.wikimedia.org/wikipedia/commons/2/22/TVN_Logo.svg" 
+        },
+        { 
+            n: "CNN Chile", 
+            u: "https://rudo.video/live/cnnchile", 
+            img: "https://upload.wikimedia.org/wikipedia/commons/6/66/CNN_Chile_logo.svg" 
         }
     ]
 };
